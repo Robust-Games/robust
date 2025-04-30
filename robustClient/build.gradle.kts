9092708004b920c754239036af97b34fbdedfe02
eyesi001 <ersin.yesiltas@student.hs-rm.de>
2025-04-30 19:40:42 +0200

    Build finished

--- robustClient/build.gradle.kts
+++ robustClient/build.gradle.kts
@@ -4,16 +4,13 @@ plugins {
     id("org.javamodularity.moduleplugin") version "1.8.12"
     id("org.openjfx.javafxplugin") version "0.0.13"
     id("org.beryx.jlink") version "2.25.0"
-    id("org.jetbrains.kotlin.jvm") version "1.5.32"
 }
 
-group = "com.robustGames"
+group = "com.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    jcenter()
-
 }
 
 val junitVersion = "5.10.2"
@@ -35,12 +32,14 @@ application {
 
 javafx {
     version = "17.0.6"
-    modules = listOf("javafx.controls", "javafx.graphics", "javafx.fxml", "javafx.media")
-    }
+    modules = listOf("javafx.controls", "javafx.fxml", "javafx.media")
+}
 
 dependencies {
-    compile("com.github.almasb:fxgl:21.1")
-    implementation("com.github.almasb:fxgl:17.1")
+    implementation("com.github.almasb:fxgl:17.3") {
+        exclude(group = "org.openjfx")
+       // exclude(group = "org.jetbrains.kotlin")
+    }
     testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
 }
@@ -55,4 +54,5 @@ jlink {
     launcher {
         name = "app"
     }
+    addExtraDependencies("kotlin.stdlib")
 }
