652ec57a2a39c2f45193e43d005c811b0bd23bac
Nico Steiner <nico.steiner@student.hs-rm.de>
2025-05-14 11:43:17 +0200

    Final Update - Beta für Isometrische map

--- robustClient/src/main/java/com/robustgames/robustclient/application/RobustApplication.java
+++ robustClient/src/main/java/com/robustgames/robustclient/application/RobustApplication.java
@@ -3,6 +3,8 @@ package com.robustgames.robustclient.application;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.GameWorld;
 import com.almasb.fxgl.entity.SpawnData;
 import com.robustgames.robustclient.business.collision.ShellCityHandler;
 import com.robustgames.robustclient.business.collision.ShellTankHandler;
@@ -12,6 +14,8 @@ import com.robustgames.robustclient.business.factories.PlayerFactory;
 import com.robustgames.robustclient.business.logic.MapService;
 import javafx.geometry.Point2D;
 import javafx.scene.input.MouseButton;
+
+import java.util.List;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
 
@@ -31,12 +35,28 @@ public class RobustApplication extends GameApplication  {
     @Override
     protected void initInput() {
         //DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
-        onBtnDown(MouseButton.PRIMARY, () -> {
-            Point2D mouseWorldPos = FXGL.getInput().getMousePositionWorld();
-            Point2D gridPos = MapService.orthScreenToGrid(mouseWorldPos);
-            System.out.println("isoScreenToGrid = " + gridPos + "\nisoGridToScreen = "
-                + MapService.orthGridToScreen(gridPos.getX(), gridPos.getY()));
-        });
+        String whatWorld = "isometric"; //orthogonal or isometric
+
+        if (whatWorld.equals("orthogonal")) {
+            onBtnDown(MouseButton.PRIMARY, () -> {
+                Point2D mouseWorldPos = FXGL.getInput().getMousePositionWorld();
+                Point2D gridPos = MapService.orthScreenToGrid(mouseWorldPos);
+                System.out.println("\nFXGL Mouse coordinates = " + mouseWorldPos
+                        + "\northogonal Screen To Grid = " + gridPos
+                        + "\northogonal Grid To Screen = " + MapService.orthGridToScreen(gridPos.getX(), gridPos.getY()));
+            });
+        }
+        else if (whatWorld.equals("isometric")) {
+            onBtnDown(MouseButton.PRIMARY, () -> {
+                Point2D mouseWorldPos = FXGL.getInput().getMousePositionWorld();
+                Point2D gridPos = MapService.isoScreenToGrid(mouseWorldPos);
+                System.out.println("\nFXGL Mouse coordinates = " + mouseWorldPos
+                        + "\nisometric Screen To Grid = " + gridPos
+                        + "\nisometric Grid To Screen = " + MapService.isoGridToScreen(gridPos.getX(), gridPos.getY())
+                        + "\northogonal Screen To Grid = " + MapService.orthScreenToGrid(mouseWorldPos)
+                        + "\northogonal Grid To Screen = " + MapService.orthGridToScreen(MapService.orthScreenToGrid(mouseWorldPos).getX(), MapService.orthScreenToGrid(mouseWorldPos).getY()));
+            });
+        }
         //DEBUG DEBUG DEBUG DEBUG DEBUG DEBUG
 
     }
@@ -61,12 +81,21 @@ public class RobustApplication extends GameApplication  {
 
     @Override
     protected void initGame() {
-        Point2D mouseWorldPos = FXGL.getInput().getMousePositionWorld();
-        Point2D gridPos = MapService.orthScreenToGrid(mouseWorldPos);
         FXGL.getGameWorld().addEntityFactory(new MapFactory());
         FXGL.getGameWorld().addEntityFactory(new PlayerFactory());
         FXGL.spawn("Background", new SpawnData(0, 0).put("width", WIDTH).put("height", HEIGHT));
-        FXGL.setLevelFromMap("map2D.tmx");
+        FXGL.setLevelFromMap("map2D.tmx"); //map2D.tmx für 2D und mapTest.tmx für Isometrisch
+
+        GameWorld world = getGameWorld();
+        //ISOMETRISCHE MAP TRIGGER ISOMETRISCHE MAP TRIGGER ISOMETRISCHE MAP TRIGGER ISOMETRISCHE MAP TRIGGER
+//        List<Entity> allEntities = world.getEntities().subList(2, world.getEntities().size());
+//        System.out.println(allEntities);
+//        for (Entity e : allEntities) {
+//            Point2D orthGridPos = MapService.orthScreenToGrid(e.getPosition());
+//            Point2D isoGridPos = MapService.isoGridToScreen(orthGridPos.getX(), orthGridPos.getY());
+//            e.setPosition(isoGridPos.getX()-32, isoGridPos.getY());
+//        }
+        //ISOMETRISCHE MAP TRIGGER ISOMETRISCHE MAP TRIGGER ISOMETRISCHE MAP TRIGGER ISOMETRISCHE MAP TRIGGER
     }
 
     public static void main(String[] args) {
--- robustClient/src/main/java/com/robustgames/robustclient/business/logic/MapService.java
+++ robustClient/src/main/java/com/robustgames/robustclient/business/logic/MapService.java
@@ -13,30 +13,21 @@ public class MapService {
     private static final int TILE_WIDTH_ORTH = 64;
     private static final int TILE_WIDTH_ISO = 128;
     private static final int TILE_HEIGHT = 64;
-    private static final int ISO_TILE_OFFSET_X = 5;
-    private static final int ISO_TILE_OFFSET_Y = 0; //aktuell 0 aber evtl. 1 in zukunft
+    private static final int ISO_TILE_ORIGIN_X = 5;
+    private static final int ISO_TILE_ORIGIN_Y = 0; //aktuell 0 aber evtl. 1 in zukunft
 
     public static Point2D isoScreenToGrid(Point2D screenPos) {
-        System.out.println("x = " + screenPos.getX());
-        System.out.println("y = " + screenPos.getY());
-        Point2D gridPos = new Point2D(screenPos.getX()/TILE_WIDTH_ISO, screenPos.getY()/TILE_HEIGHT);
-        System.out.println("gridPos = " + gridPos);
-        int x = (int) gridPos.getX() - ISO_TILE_OFFSET_X;
-        int y = (int) gridPos.getY() - ISO_TILE_OFFSET_Y;
-        int gridX =  y+x;
-        int gridY = y-x;
-        return new Point2D(gridX, gridY);
+        Point2D gridPos2D = new Point2D(screenPos.getX()/TILE_WIDTH_ISO, screenPos.getY()/TILE_HEIGHT);
+        int x = (int) ((gridPos2D.getY() - ISO_TILE_ORIGIN_Y) + (gridPos2D.getX() - ISO_TILE_ORIGIN_X));
+        int y = (int) ((gridPos2D.getY() - ISO_TILE_ORIGIN_Y) - (gridPos2D.getX() - ISO_TILE_ORIGIN_X));
+        return new Point2D(x, y);
     }
     // isometric grid indices to screen (not world) coordinates, so it can draw stuff correctly
     public static Point2D isoGridToScreen(double gridX, double gridY) {
-        double worldX = (gridX - gridY) * (TILE_WIDTH_ISO /2.0); //08:34 im video
-        double worldY = (gridX + gridY) * (TILE_HEIGHT/2.0);
-        worldX += ISO_TILE_OFFSET_X * TILE_WIDTH_ISO; //damit die koordinaten ab Zentrum der Karte anfangen
-        worldY += ISO_TILE_OFFSET_Y * TILE_WIDTH_ISO;
-        return new Point2D(worldX, worldY);
+        double screenX = (ISO_TILE_ORIGIN_X * TILE_WIDTH_ISO) + (gridX - gridY) * (TILE_WIDTH_ISO /2.0);
+        double screenY = ISO_TILE_ORIGIN_Y * TILE_HEIGHT + (gridX + gridY) * (TILE_HEIGHT /2.0);
+        return new Point2D(screenX, screenY);
     }
-
-
 
     public static Point2D orthScreenToGrid(Point2D screenPos) {
         int gridX = (int) (screenPos.getX() / TILE_WIDTH_ORTH);
@@ -46,10 +37,11 @@ public class MapService {
 
     // grid indices to world coordinates for spawning stuff
     public static Point2D orthGridToScreen(double gridX, double gridY) {
-        double worldX = gridX * TILE_WIDTH_ORTH + TILE_WIDTH_ORTH / 2.0;
-        double worldY = gridY * TILE_HEIGHT + TILE_HEIGHT / 2.0;
+        double worldX = gridX * TILE_WIDTH_ORTH;
+        double worldY = gridY * TILE_HEIGHT;
         return new Point2D(worldX, worldY);
     }
+
     public static Entity findSelectedTank(){
         for (Entity e : FXGL.getGameWorld().getEntities()) {
             if (e.hasComponent(SelectableComponent.class))
