dae9621520891c1e51061dd932fa26f44362c98b
Burak Abi <buxxkalxxn@gmail.com>
2025-05-16 10:49:23 +0200

    Panzerrotierung mit WASD-Tasten möglich

--- robustClient/src/main/java/com/robustgames/robustclient/business/entitiy/components/RotateComponent.java
+++ robustClient/src/main/java/com/robustgames/robustclient/business/entitiy/components/RotateComponent.java
@@ -2,29 +2,37 @@ package com.robustgames.robustclient.business.entitiy.components;
 
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.component.Component;
-import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
+import java.util.List;
 
 public class RotateComponent extends Component {
 
     public void rotateUp()    {
-        rotateTo("tank2D_up.png");    }
+        rotateTo("tank2D_up.png");
+    }
     public void rotateDown()  {
-        rotateTo("tank2D_down.png");  }
+        rotateTo("tank2D_down.png");
+    }
     public void rotateLeft()  {
-        rotateTo("tank2D_left.png");  }
+        rotateTo("tank2D_left.png");
+    }
     public void rotateRight() {
-        rotateTo("tank2D_right.png"); }
+        rotateTo("tank2D_right.png");
+    }
 
-    void rotateTo(String png ){
-        // altes Tank‐Bild entfernen (aber nicht selection.png)
-        entity.getViewComponent().getChildren().removeIf(node ->
-                node instanceof ImageView &&
-                        ((ImageView) node).getImage().getUrl().endsWith("png")
-        );
+    void rotateTo(String png ){ // Uneffizient ?
+        List<Node> ch = entity.getViewComponent().getChildren();
 
-        // neues Bild laden und hinzufügen
+        for(Node e: ch) {
+            if(e instanceof ImageView iv){
+                if(iv.getImage().getUrl().contains("tank2D")){
+                    entity.getViewComponent().removeChild(e);
+                }
+
+                }
+        }
+
         Node img = FXGL.getAssetLoader().loadTexture(png);
         entity.getViewComponent().addChild(img);
     }
