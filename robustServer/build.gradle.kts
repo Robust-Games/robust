4e28c0f84b934935899f190d6dead98ec25c528f
Carolin Scheffler <Carolin.Scheffler@student.hs-rm.de>
2025-05-18 09:43:02 +0200

    CI/CD-Pipeline, angepasste build.gradle für FXGL-Tests

--- robustServer/build.gradle.kts
+++ robustServer/build.gradle.kts
@@ -2,7 +2,10 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
+    id("jacoco")
+    id("com.github.ben-manes.versions") version "0.48.0"
 }
+
 
 group = "com.robustGames"
 version = "0.0.1-SNAPSHOT"
@@ -29,8 +32,14 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.testfx:testfx-junit5:4.0.16-alpha")
+    testImplementation("com.almasb:fxgl-test:11.17")
 }
 
-tasks.withType<Test> {
+tasks.test {
     useJUnitPlatform()
 }
+
+
+
+
