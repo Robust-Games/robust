d5e422fb9f17031e5c99a34879ad3ab832136363
Cluelessoreo <92376554+Cluelessoreo@users.noreply.github.com>
2025-05-18 12:51:50 +0200

    Update EntityType.java
    testo

--- robustClient/src/main/java/com/robustgames/robustclient/business/entitiy/EntityType.java
+++ robustClient/src/main/java/com/robustgames/robustclient/business/entitiy/EntityType.java
@@ -2,4 +2,5 @@ package com.robustgames.robustclient.business.entitiy;
 
 public enum EntityType {
     CITY, TANK, SHELL, TILE //evtl noch POWERUP oder andere Fahrzeuge, wenn wir dazu kommen
-}
+//testo
+    }
