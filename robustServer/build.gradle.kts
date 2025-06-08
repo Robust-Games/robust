a69824ac67305c5041e57ee96d8d7228f3c0f11a
eyesi001 <ersin.yesiltas@student.hs-rm.de>
2025-06-08 11:20:29 +0200

    unfertiger fxgl server

--- robustServer/build.gradle.kts
+++ robustServer/build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.5"
-    id("io.spring.dependency-management") version "1.1.7"
     id("jacoco")
     id("com.github.ben-manes.versions") version "0.48.0"
 }
-
 
 group = "com.robustGames"
 version = "0.0.1-SNAPSHOT"
@@ -16,21 +13,14 @@ java {
     }
 }
 
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
-
 repositories {
     mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("com.github.almasb.fxgl:fxgl:11.17")
+    implementation("com.github.almasb.fxgl:fxgl-network:11.17")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.testfx:testfx-junit5:4.0.16-alpha")
     testImplementation("com.almasb:fxgl-test:11.17")
@@ -39,7 +29,3 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
-
-
-
-
--- robustServer/src/main/java/com/robustgames/robustserver/RobustServerApplication.java
+++ robustServer/src/main/java/com/robustgames/robustserver/RobustServerApplication.java
@@ -1,13 +1,39 @@
 package com.robustgames.robustserver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.net.NetServer;
+import com.almasb.fxgl.net.NetworkService;
 
-@SpringBootApplication
-public class RobustServerApplication {
+public class RobustServerApplication extends GameApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RobustServerApplication.class, args);
+    private NetServer<Object> server;
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setTitle("Robust Server");
+        settings.setWidth(400);
+        settings.setHeight(300);
     }
 
+    @Override
+    protected void initGame() {
+        // Hole den NetworkService aus FXGL
+        NetworkService network = getGameScene().getApplication().getService(NetworkService.class);
+
+        // Erstelle einen NetServer (Objekttyp für serialisierte Messages)
+        server = network.newNetServer(7777, Object.class);
+
+        // Beispiel-Listener für Messages
+        server.addMessageHandler(Object.class, (conn, data) -> {
+            System.out.println("Received from client: " + data);
+        });
+
+        System.out.println("FXGL-NetServer läuft auf Port 7777!");
+        server.startAsync();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
